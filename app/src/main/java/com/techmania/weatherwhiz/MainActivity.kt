ff66e855d48bc0e5f5fda8ed797d1d7cef7f2065
AKHIL KUMAR <akhilkumar200485@gmail.com>
2024-04-15 15:17:39 +0530

    Weather App

--- app/src/main/java/com/techmania/weatherwhiz/MainActivity.kt
+++ app/src/main/java/com/techmania/weatherwhiz/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-   fetchWeatherData("shimla")
+   fetchWeatherData(" ")
         searchCity()
     }
 
--- app/src/main/java/com/techmania/weatherwhiz/SplashActivity.kt
+++ app/src/main/java/com/techmania/weatherwhiz/SplashActivity.kt
@@ -5,16 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.techmania.weatherwhiz.databinding.ActivityMainBinding
+import com.techmania.weatherwhiz.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    private val binding: ActivitySplashBinding by lazy {
+        ActivitySplashBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(binding.root)
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
+            binding.lottieAnimationViewSP.setAnimation(R.raw.splash1ani)
+            binding.lottieAnimationViewSP.playAnimation()
+            binding.lottieAnimationView2SP.setAnimation(R.raw.splash2ani)
+            binding.lottieAnimationView2SP.playAnimation()
             finish()
 
-        },3000)
+        },4000)
     }
 }
